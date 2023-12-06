7f252c39afd1102ef1edabc138c5480105030042
lohengrin <oljn22@student.bth.se>
2023-12-06 17:14:33 +0100

    shortcuts

--- assets/cimage/img_config.php
+++ assets/cimage/img_config.php
@@ -1,8 +1,17 @@
 <?php
+
 return [
     "mode"         => "development",
     "image_path"   =>  __DIR__ . "/../../assets/img/",
     "cache_path"   =>  __DIR__ . "/../../cache/cimage/",
     "autoloader"   =>  __DIR__ . "/../../vendor/autoload.php",
     "windows2wsl"  => true,
+    "shortcut" => array(
+        "sepia" =>"&f=grayscale&f0=brightness,-10&f1=contrast,-20&f2=colorize,120,60,0,0&sharpen",
+        "portrait" => "&w=440&crop-to-fit&aspect-ratio=!4:3&q=70&sharpen",
+        "square-sm" => "&w=230&h=230&crop-to-fit&q=70&sharpen",
+        "square-lg" => "&w=300&h=300&crop-to-fit&q=70&sharpen",
+        "screenshot-sm" => "&w=440&h=224&crop-to-fit&crop=1200,600,left,top&save-as=jpg",
+        "screenshot-lg" => "&w=610&h=224&crop-to-fit&crop=1200,430,left,top&save-as=jpg"
+    ),
 ];
